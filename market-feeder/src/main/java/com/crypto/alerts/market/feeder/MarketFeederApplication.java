c2e7f6c629cb8e9cd529367e443631b7e8b37454
edmundkb <edmundkb@outlook.com>
2026-06-03 00:36:18 +0100

    initial commit

---
+++ market-feeder/src/main/java/com/crypto/alerts/market/feeder/MarketFeederApplication.java
@@ -0,0 +1,13 @@
+package com.crypto.alerts.market.feeder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketFeederApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketFeederApplication.class, args);
+	}
+
+}
---
+++ market-feeder/src/test/java/com/crypto/alerts/market/feeder/MarketFeederApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crypto.alerts.market.feeder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketFeederApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
